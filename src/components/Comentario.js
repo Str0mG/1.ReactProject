b04702d18a92b8557ab4413a6773e43326e8a577
Gabriel Trombini <gabrieltrombini793@gmail.com>
2022-07-05 14:50:08 -0300

    add: components

---
+++ src/components/Comentario.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Comentario.css';
+
+
+const Comentario = () => (
+    <div className="Comentario">
+        <div>Olá Gabriel</div>
+        <div>Tudo bem?</div>
+    </div>  
+);
+
+export default Comentario;
